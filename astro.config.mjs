198cade8262fd6e1fe74c29aa5de618ee17cfed3
mrkirby153 <mr.austinwhyte@gmail.com>
2024-07-08 21:05:23 -0700

    Use Expressive Code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,24 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import expressiveCode from "astro-expressive-code";
+import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.mrkirby153.com",
-  integrations: [mdx(), tailwind(), react()],
+  integrations: [
+    expressiveCode({
+      plugins: [pluginLineNumbers()],
+      defaultProps: {
+        showLineNumbers: false,
+      },
+    }),
+    mdx(),
+    tailwind(),
+    react(),
+  ],
   redirects: {
     "/posts": "/posts/1",
   },
