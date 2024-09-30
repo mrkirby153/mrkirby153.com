705b4fd9af012c30feb7d42f9c025c93edbf4459
mrkirby153 <mr.austinwhyte@gmail.com>
2024-09-29 20:02:09 -0700

    Prose tweaks
    - Add typography
    - Support latex

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,10 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import expressiveCode from "astro-expressive-code";
 import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
+import remarkToc from "remark-toc";
+import { rehypeAccessibleEmojis } from "rehype-accessible-emojis";
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,8 +19,15 @@ export default defineConfig({
         showLineNumbers: false,
       },
     }),
-    mdx(),
+    mdx({
+      remarkPlugins: [remarkMath, remarkToc],
+      rehypePlugins: [rehypeKatex, rehypeAccessibleEmojis],
+    }),
     tailwind(),
     react(),
   ],
+  markdown: {
+    remarkPlugins: [remarkMath, remarkToc],
+    rehypePlugins: [rehypeKatex, rehypeAccessibleEmojis],
+  },
 });
