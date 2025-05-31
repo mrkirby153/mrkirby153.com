f03f473f72bface85820ca0f0a8bb32c7d0a1107
mrkirby153 <mr.austinwhyte@gmail.com>
2025-05-31 10:54:22 -0700

    Use Tailwind v4 correctly

---
+++ src/styles/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            p: {
+              marginTop: "0",
+              marginBottom: "0",
+            },
+          },
+        },
+      },
+    },
+  },
+};
